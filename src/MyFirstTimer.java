beadee7d3e1dc74ee34637520df13458196af4ab
TYTengYuan <t_yuan2@u.pacific.edu>
2025-02-23 20:41:09 -0800

    Added a Timer with an initial delay, an ActionListener, and a start command. The label moves every second and stops after 10 updates.

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,11 +1,18 @@
-import acm.graphics.*;
-import acm.program.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyFirstTimer extends GraphicsProgram {
+import javax.swing.Timer;
+
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
+
+public class MyFirstTimer extends GraphicsProgram implements ActionListener{
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
+	private Timer timer;
+	private int numTimes;
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -15,6 +22,22 @@ public class MyFirstTimer extends GraphicsProgram {
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		
+		numTimes = 0;
+		
+		timer = new Timer(1000, this);
+		timer.setInitialDelay(3000);
+		timer.start();
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		numTimes++;
+		myLabel.setLabel("times called? " + numTimes);
+		myLabel.move(5, 0);
+		
+		if (numTimes >= 10) {
+			timer.stop();
+		}
 	}
 	
 	public static void main(String[] args) {
